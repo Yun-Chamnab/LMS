2af20f130262a22ddc9c12fa3928539f9827bc85
simhornlimhor <simhornlimhor@gmail.com>
2021-02-20 15:06:10 +0700

    authentication API

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Pearl\RequestValidate\RequestAbstract;
+
+class LoginRequest extends RequestAbstract
+{
+    /**
+    * Sanitize request data before validation.
+    */
+    protected function prepareForValidation()
+    {
+        $this->merge(['email' => strtoLower($this->input('email'))]);
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required',
+            'password' => 'required',
+        ];
+    }
+}
